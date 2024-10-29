70b6dd16b7fbac80c30639054332503f6c74761a
Maestro3000 <melnik.igor.s@gmail.com>
2024-10-30 00:23:23 +0400

    Forum cart

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,10 +3,10 @@ import { ChapterPage } from '@/pages/Chapter/chapter'
 import { ThemePage } from '@/pages/Theme'
 import { WarrantorPage } from '@/pages/Warrantor'
 import { Path } from '@/shared/model'
+import {ComentTheme} from "@/widgets/ComentTheme"
 // import { Profile } from '@/layout/Profile'
 import { ChaptersBlock } from '@/widgets/ChaptersBlock'
 import { createBrowserRouter } from 'react-router-dom'
-
 export const router = createBrowserRouter([
 	{
 		path: Path.BASE,
@@ -27,6 +27,10 @@ export const router = createBrowserRouter([
 			{
 				path: Path.THEME,
 				element: <ThemePage />,
+			},
+			{
+				path: Path.TEST,
+				element: <ComentTheme />,
 			},
 		],
 	},
